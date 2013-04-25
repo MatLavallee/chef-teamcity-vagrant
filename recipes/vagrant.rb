29ab793375e63e96c5a865b60109da0d3ed65ab2
Mathieu Levesque-Lavallee <mathieu.levesque-lavallee@datahex.ca>
2013-04-25 11:10:28 -0400

    Install vagrant with deb package

--- recipes/vagrant.rb
+++ recipes/vagrant.rb
@@ -1,3 +1,12 @@
 include_recipe 'apt'
 
-package 'vagrant'
+deb_path = '/tmp/vagrant.deb'
+
+remote_file deb_path do
+  source 'http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/vagrant_1.2.2_i686.deb'
+  mode 0777
+end
+
+dpkg_package deb_path do
+  action :install
+end
