7dbd9cd6709408832de18e0ba3d6945f2c1c7a0b
Mathieu Levesque-Lavallee <mathieu.levesque-lavallee@datahex.ca>
2013-04-26 13:26:07 -0400

    install virtual box with package

--- recipes/virtualbox.rb
+++ recipes/virtualbox.rb
@@ -1,18 +1,5 @@
 include_recipe 'apt'
 
-# Install headers
-package 'linux-headers-3.2.0-23-generic-pae'
-
-# Download virtualBox
-deb_path = '/tmp/virtualbox.deb'
-remote_file deb_path do
-  source 'http://download.virtualbox.org/virtualbox/4.2.12/virtualbox-4.2_4.2.12-84980~Ubuntu~precise_i386.deb'
-  mode 0777
-end
-
-# Install VirtualBox
-dpkg_package deb_path do
-  action :install
-end
-
-
+package 'virtualbox'
+#execute 'apt-get install kernel-headers-$(uname -r)'
+#package 'virtualbox-dkms'
