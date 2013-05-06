b36413f3c553dc21a179c04ca52278f5fa93847e
Mathieu Levesque-Lavallee <mathieu.levesque-lavallee@datahex.ca>
2013-05-06 15:35:24 -0400

    Add default teamcity attributes

--- attributes/teamcity.rb
+++ attributes/teamcity.rb
@@ -1,5 +1,6 @@
 default['teamcity']['version'] = '7.1.4'
 default['teamcity']['install_dir'] = '/opt/'
 default['teamcity']['data_path'] = '/var/TeamCity/.BuildServer'
+default['teamcity']['user']['password'] = '1234'
 
-default['java']['oracle']['accept_oracle_download_terms'] = true
+node['java']['oracle']['accept_oracle_download_terms'] = true
