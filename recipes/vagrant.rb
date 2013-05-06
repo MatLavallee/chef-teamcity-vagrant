c5f592dbe5bebe955c64afd1d1de247e864e6b1c
Mathieu Levesque-Lavallee <mathieu.levesque-lavallee@datahex.ca>
2013-05-06 15:36:29 -0400

    Use i686 vagrant

--- recipes/vagrant.rb
+++ recipes/vagrant.rb
@@ -3,7 +3,7 @@ include_recipe 'apt'
 deb_path = '/tmp/vagrant.deb'
 
 remote_file deb_path do
-  source 'http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/vagrant_1.2.2_x86_64.deb'
+  source 'http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/vagrant_1.2.2_i686.deb'
   mode 0777
 end
 
