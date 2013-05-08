724e50f4dd1ca26378d75c30fde4a0825c31e122
Mathieu Levesque-Lavallee <mathieu.levesque-lavallee@datahex.ca>
2013-05-08 10:57:02 -0400

    Run teamcity as a new user

--- recipes/teamcity.rb
+++ recipes/teamcity.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-#teamcity_user = 'teamcity'
+teamcity_user = node['teamcity']['user']['name']
 version = node['teamcity']['version']
 install_dir = node['teamcity']['install_dir']
 install_path = File.join install_dir, 'TeamCity'
@@ -16,12 +16,13 @@ data_path = node['teamcity']['data_path']
 # Install Java
 include_recipe 'java::oracle'
 
-## Create user
-#user teamcity_user do
-#  comment 'User running TeamCity'
-#  home install_path
-#  shell '/bin/bash'
-#end
+# Create user
+user teamcity_user do
+  comment 'User running TeamCity'
+  shell '/bin/bash'
+  home "/home/#{teamcity_user}"
+  supports({ manage_home: true })
+end
 
 # Download Teamcity
 download_path = File.join install_dir, "TeamCity-#{version}.tar.gz"
@@ -29,30 +30,17 @@ download_path = File.join install_dir, "TeamCity-#{version}.tar.gz"
 remote_file download_path do
   source "http://download.jetbrains.com/teamcity/TeamCity-#{version}.tar.gz"
   action :create_if_missing
+  owner teamcity_user
+  group teamcity_user
 end
 
 # Unpack
 execute 'Unpack package' do
   command "tar -xvzf #{download_path} --directory \"#{install_dir}\""
   creates install_path
+  user teamcity_user
+  group teamcity_user
 end
-
-## Change install and data directories owner and group
-#[install_path, data_path].each do |dir|
-#  directory dir do
-#    owner teamcity_user
-#    group teamcity_user
-#    recursive true
-#  end
-#  execute "Change owner of #{dir}" do
-#    command "chown -R #{teamcity_user} #{dir}"
-#    only_if { ::File.exists?(dir) }
-#  end
-#  execute "Change group of #{dir}" do
-#    command "chgrp -R #{teamcity_user} #{dir}"
-#    only_if { ::File.exists?(dir) }
-#  end
-#end
 
 # Install as a service
 template '/etc/init.d/teamcity' do
@@ -60,7 +48,7 @@ template '/etc/init.d/teamcity' do
   variables(
       :teamcity_data_path => data_path,
       :teamcity_path => install_path,
-      #:user => teamcity_user
+      :user => teamcity_user
   )
   owner 'root'
   group 'root'
