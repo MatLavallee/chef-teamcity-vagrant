5a55f9185880a44e7a590dcca8abb17fad04f907
Mathieu Levesque-Lavallee <mathieu.levesque-lavallee@datahex.ca>
2013-05-08 10:26:48 -0400

    Install virtual box

--- recipes/virtualbox.rb
+++ recipes/virtualbox.rb
@@ -1,5 +1,21 @@
 include_recipe 'apt'
 
-package 'virtualbox'
-#execute 'apt-get install kernel-headers-$(uname -r)'
-#package 'virtualbox-dkms'
+%w[
+  virtualbox
+  virtualbox-dkms
+  virtualbox-ose-dkms
+].each do |pkg|
+  package pkg do
+    notifies :run, 'execute[reconfigure-virtualbox]'
+  end
+end
+
+execute 'install-headers' do
+  command 'apt-get -y install linux-headers-$(uname -r)'
+end
+
+execute 'reconfigure-virtualbox' do
+  command 'dpkg-reconfigure virtualbox-dkms virtualbox'
+  action :nothing
+end
+
