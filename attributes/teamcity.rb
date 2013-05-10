0f01fd0f19e81dee8b4ec002a40d786f4698e8b8
Mathieu Levesque-Lavallee <mathieu.levesque-lavallee@datahex.ca>
2013-05-10 14:00:15 -0400

    Accept oracle

--- attributes/teamcity.rb
+++ attributes/teamcity.rb
@@ -3,3 +3,5 @@ default['teamcity']['install_dir'] = '/home/teamcity/'
 default['teamcity']['data_path'] = '/home/teamcity/TeamCityData'
 default['teamcity']['java_home'] = '/usr/lib/jvm/default-java'
 default['teamcity']['user']['name'] = 'teamcity'
+
+default['java']['oracle']['accept_oracle_download_terms'] = true
